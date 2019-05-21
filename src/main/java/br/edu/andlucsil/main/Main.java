c293c5cb2deda2c5f72b858d751ab3416cfb014a
andre lucas <andlucsil1221@gmail.com>
2019-05-21 19:50:23 -0300

    alterado nome do pacote Main

--- src/main/java/br/edu/andlucsil/main/Main.java
+++ src/main/java/br/edu/andlucsil/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.andlucsil.main.br.edu.andlucsil.main;
+package br.edu.andlucsil.main;
 
 import br.edu.andlucsil.serial.SerialComm;
 /**
